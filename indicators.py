258a7fd431b63f842fe8c6ac331dc5da7ae149d0
L1TANK <91131045+L1TANK@users.noreply.github.com>
2024-05-30 19:31:08 +0300

    number1

---
+++ indicators.py
@@ -0,0 +1,7 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, Input, Output, dcc, html
+from Pages import all_map, country, indicators
+from data import df, all_cont
+from dash import html, dcc, callback, Output, Input
+import plotly.express as px
+
